0992ed498dd3e300c2ecd921a5fe442e5a2c61fb
Michael Cooper <CPM@braybrooksc.vic.edu.au>
2021-03-09 15:31:33 +1100

    Data saves to XML

---
+++ mem_database/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿
+namespace mem_database
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HeadingLabel = new System.Windows.Forms.Label();
+            this.idHeadingLabel = new System.Windows.Forms.Label();
+            this.idLAbel = new System.Windows.Forms.Label();
+            this.firstNameHeadingLabel = new System.Windows.Forms.Label();
+            this.DOBheadingLabel = new System.Windows.Forms.Label();
+            this.surnameHeadingLabel = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.GenderHeadingLabel = new System.Windows.Forms.Label();
+            this.MobNoHeadingLabel = new System.Windows.Forms.Label();
+            this.AddressHeadingLabel = new System.Windows.Forms.Label();
+            this.FirstNameTextbox = new System.Windows.Forms.TextBox();
+            this.SurnameTextBox = new System.Windows.Forms.TextBox();
+            this.AddressTextbox = new System.Windows.Forms.TextBox();
+            this.MobNoTextbox = new System.Windows.Forms.TextBox();
+            this.GenderComboBox = new System.Windows.Forms.ComboBox();
+            this.PaidStatusHeadingLabel = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.AmountToPayHeadingLabel = new System.Windows.Forms.Label();
+            this.AmountToPayLabel = new System.Windows.Forms.Label();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // HeadingLabel
+            // 
+            this.HeadingLabel.AutoSize = true;
+            this.HeadingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HeadingLabel.Location = new System.Drawing.Point(682, 14);
+            this.HeadingLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.HeadingLabel.Name = "HeadingLabel";
+            this.HeadingLabel.Size = new System.Drawing.Size(389, 25);
+            this.HeadingLabel.TabIndex = 0;
+            this.HeadingLabel.Text = "Sporting club membership database";
+            // 
+            // idHeadingLabel
+            // 
+            this.idHeadingLabel.AutoSize = true;
+            this.idHeadingLabel.Location = new System.Drawing.Point(152, 149);
+            this.idHeadingLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.idHeadingLabel.Name = "idHeadingLabel";
+            this.idHeadingLabel.Size = new System.Drawing.Size(30, 20);
+            this.idHeadingLabel.TabIndex = 1;
+            this.idHeadingLabel.Text = "ID:";
+            // 
+            // idLAbel
+            // 
+            this.idLAbel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.idLAbel.Location = new System.Drawing.Point(194, 142);
+            this.idLAbel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.idLAbel.Name = "idLAbel";
+            this.idLAbel.Size = new System.Drawing.Size(299, 34);
+            this.idLAbel.TabIndex = 2;
+            // 
+            // firstNameHeadingLabel
+            // 
+            this.firstNameHeadingLabel.AutoSize = true;
+            this.firstNameHeadingLabel.Location = new System.Drawing.Point(98, 225);
+            this.firstNameHeadingLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.firstNameHeadingLabel.Name = "firstNameHeadingLabel";
+            this.firstNameHeadingLabel.Size = new System.Drawing.Size(86, 20);
+            this.firstNameHeadingLabel.TabIndex = 3;
+            this.firstNameHeadingLabel.Text = "FirstName:";
+            // 
+            // DOBheadingLabel
+            // 
+            this.DOBheadingLabel.AutoSize = true;
+            this.DOBheadingLabel.Location = new System.Drawing.Point(98, 369);
+            this.DOBheadingLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.DOBheadingLabel.Name = "DOBheadingLabel";
+            this.DOBheadingLabel.Size = new System.Drawing.Size(86, 20);
+            this.DOBheadingLabel.TabIndex = 7;
+            this.DOBheadingLabel.Text = "FirstName:";
+            // 
+            // surnameHeadingLabel
+            // 
+            this.surnameHeadingLabel.AutoSize = true;
+            this.surnameHeadingLabel.Location = new System.Drawing.Point(105, 297);
+            this.surnameHeadingLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.surnameHeadingLabel.Name = "surnameHeadingLabel";
+            this.surnameHeadingLabel.Size = new System.Drawing.Size(78, 20);
+            this.surnameHeadingLabel.TabIndex = 5;
+            this.surnameHeadingLabel.Text = "Surname:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(194, 363);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(298, 26);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // GenderHeadingLabel
+            // 
+            this.GenderHeadingLabel.AutoSize = true;
+            this.GenderHeadingLabel.Location = new System.Drawing.Point(105, 588);
+            this.GenderHeadingLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.GenderHeadingLabel.Name = "GenderHeadingLabel";
+            this.GenderHeadingLabel.Size = new System.Drawing.Size(67, 20);
+            this.GenderHeadingLabel.TabIndex = 13;
+            this.GenderHeadingLabel.Text = "Gender:";
+            // 
+            // MobNoHeadingLabel
+            // 
+            this.MobNoHeadingLabel.AutoSize = true;
+            this.MobNoHeadingLabel.Location = new System.Drawing.Point(98, 512);
+            this.MobNoHeadingLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.MobNoHeadingLabel.Name = "MobNoHeadingLabel";
+            this.MobNoHeadingLabel.Size = new System.Drawing.Size(64, 20);
+            this.MobNoHeadingLabel.TabIndex = 11;
+            this.MobNoHeadingLabel.Text = "MobNo:";
+            // 
+            // AddressHeadingLabel
+            // 
+            this.AddressHeadingLabel.AutoSize = true;
+            this.AddressHeadingLabel.Location = new System.Drawing.Point(111, 435);
+            this.AddressHeadingLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.AddressHeadingLabel.Name = "AddressHeadingLabel";
+            this.AddressHeadingLabel.Size = new System.Drawing.Size(72, 20);
+            this.AddressHeadingLabel.TabIndex = 9;
+            this.AddressHeadingLabel.Text = "Address:";
+            // 
+            // FirstNameTextbox
+            // 
+            this.FirstNameTextbox.Location = new System.Drawing.Point(194, 218);
+            this.FirstNameTextbox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.FirstNameTextbox.Name = "FirstNameTextbox";
+            this.FirstNameTextbox.Size = new System.Drawing.Size(298, 26);
+            this.FirstNameTextbox.TabIndex = 14;
+            // 
+            // SurnameTextBox
+            // 
+            this.SurnameTextBox.Location = new System.Drawing.Point(194, 291);
+            this.SurnameTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.SurnameTextBox.Name = "SurnameTextBox";
+            this.SurnameTextBox.Size = new System.Drawing.Size(298, 26);
+            this.SurnameTextBox.TabIndex = 15;
+            // 
+            // AddressTextbox
+            // 
+            this.AddressTextbox.Location = new System.Drawing.Point(194, 435);
+            this.AddressTextbox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.AddressTextbox.Name = "AddressTextbox";
+            this.AddressTextbox.Size = new System.Drawing.Size(298, 26);
+            this.AddressTextbox.TabIndex = 16;
+            // 
+            // MobNoTextbox
+            // 
+            this.MobNoTextbox.Location = new System.Drawing.Point(194, 508);
+            this.MobNoTextbox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MobNoTextbox.Name = "MobNoTextbox";
+            this.MobNoTextbox.Size = new System.Drawing.Size(298, 26);
+            this.MobNoTextbox.TabIndex = 17;
+            // 
+            // GenderComboBox
+            // 
+            this.GenderComboBox.FormattingEnabled = true;
+            this.GenderComboBox.Items.AddRange(new object[] {
+            "Male",
+            "Female",
+            "Unspecified"});
+            this.GenderComboBox.Location = new System.Drawing.Point(194, 588);
+            this.GenderComboBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.GenderComboBox.MaxDropDownItems = 3;
+            this.GenderComboBox.Name = "GenderComboBox";
+            this.GenderComboBox.Size = new System.Drawing.Size(298, 28);
+            this.GenderComboBox.TabIndex = 18;
+            // 
+            // PaidStatusHeadingLabel
+            // 
+            this.PaidStatusHeadingLabel.AutoSize = true;
+            this.PaidStatusHeadingLabel.Location = new System.Drawing.Point(118, 642);
+            this.PaidStatusHeadingLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.PaidStatusHeadingLabel.Name = "PaidStatusHeadingLabel";
+            this.PaidStatusHeadingLabel.Size = new System.Drawing.Size(44, 20);
+            this.PaidStatusHeadingLabel.TabIndex = 19;
+            this.PaidStatusHeadingLabel.Text = "Paid:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(194, 642);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(55, 24);
+            this.radioButton1.TabIndex = 20;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Yes";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(366, 642);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 24);
+            this.radioButton2.TabIndex = 21;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "No";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // AmountToPayHeadingLabel
+            // 
+            this.AmountToPayHeadingLabel.AutoSize = true;
+            this.AmountToPayHeadingLabel.Location = new System.Drawing.Point(111, 698);
+            this.AmountToPayHeadingLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.AmountToPayHeadingLabel.Name = "AmountToPayHeadingLabel";
+            this.AmountToPayHeadingLabel.Size = new System.Drawing.Size(65, 20);
+            this.AmountToPayHeadingLabel.TabIndex = 22;
+            this.AmountToPayHeadingLabel.Text = "Amount";
+            // 
+            // AmountToPayLabel
+            // 
+            this.AmountToPayLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.AmountToPayLabel.Location = new System.Drawing.Point(194, 698);
+            this.AmountToPayLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.AmountToPayLabel.Name = "AmountToPayLabel";
+            this.AmountToPayLabel.Size = new System.Drawing.Size(299, 34);
+            this.AmountToPayLabel.TabIndex = 23;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(230, 789);
+            this.SaveButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(195, 112);
+            this.SaveButton.TabIndex = 24;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1948, 1421);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.AmountToPayLabel);
+            this.Controls.Add(this.AmountToPayHeadingLabel);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.PaidStatusHeadingLabel);
+            this.Controls.Add(this.GenderComboBox);
+            this.Controls.Add(this.MobNoTextbox);
+            this.Controls.Add(this.AddressTextbox);
+            this.Controls.Add(this.SurnameTextBox);
+            this.Controls.Add(this.FirstNameTextbox);
+            this.Controls.Add(this.GenderHeadingLabel);
+            this.Controls.Add(this.MobNoHeadingLabel);
+            this.Controls.Add(this.AddressHeadingLabel);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.DOBheadingLabel);
+            this.Controls.Add(this.surnameHeadingLabel);
+            this.Controls.Add(this.firstNameHeadingLabel);
+            this.Controls.Add(this.idLAbel);
+            this.Controls.Add(this.idHeadingLabel);
+            this.Controls.Add(this.HeadingLabel);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label HeadingLabel;
+        private System.Windows.Forms.Label idHeadingLabel;
+        private System.Windows.Forms.Label idLAbel;
+        private System.Windows.Forms.Label firstNameHeadingLabel;
+        private System.Windows.Forms.Label DOBheadingLabel;
+        private System.Windows.Forms.Label surnameHeadingLabel;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label GenderHeadingLabel;
+        private System.Windows.Forms.Label MobNoHeadingLabel;
+        private System.Windows.Forms.Label AddressHeadingLabel;
+        private System.Windows.Forms.TextBox FirstNameTextbox;
+        private System.Windows.Forms.TextBox SurnameTextBox;
+        private System.Windows.Forms.TextBox AddressTextbox;
+        private System.Windows.Forms.TextBox MobNoTextbox;
+        private System.Windows.Forms.ComboBox GenderComboBox;
+        private System.Windows.Forms.Label PaidStatusHeadingLabel;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label AmountToPayHeadingLabel;
+        private System.Windows.Forms.Label AmountToPayLabel;
+        private System.Windows.Forms.Button SaveButton;
+    }
+}
+
---
+++ mem_database/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+
+namespace mem_database
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            CheckDatabaseExists();
+        }
+
+        private void CheckDatabaseExists()
+        {
+            if (File.Exists("database.xml") == false)
+
+            {
+                CreateXMLandSaveData();
+            }
+            else if (File.Exists("datbase.xml") == true)
+            {
+                //SaveData();
+            }
+        }
+
+        private void CreateXMLandSaveData()
+        {
+            XmlTextWriter xWriter = new XmlTextWriter("database.xml", Encoding.UTF8);
+            xWriter.Formatting = Formatting.Indented;
+
+            xWriter.WriteStartElement("members");
+            xWriter.WriteStartElement("member");
+            xWriter.WriteStartElement("firstname");
+            xWriter.WriteString(FirstNameTextbox.Text);
+            xWriter.WriteEndElement();
+
+            xWriter.WriteStartElement("surname");
+            xWriter.WriteString(SurnameTextBox.Text);
+            xWriter.WriteEndElement();
+
+            xWriter.WriteStartElement("dob");
+            xWriter.WriteString(dateTimePicker1.Value.ToShortDateString());
+            xWriter.WriteEndElement();
+
+            xWriter.WriteStartElement("address");
+            xWriter.WriteString(AddressTextbox.Text);
+            xWriter.WriteEndElement();
+
+            xWriter.WriteStartElement("mobNo");
+            xWriter.WriteString(MobNoTextbox.Text);
+            xWriter.WriteEndElement();
+
+            xWriter.WriteStartElement("gender");
+            xWriter.WriteString(GenderComboBox.Text);
+            xWriter.WriteEndElement();
+
+            xWriter.WriteStartElement("paid");
+            xWriter.WriteString("yes");
+            xWriter.WriteEndElement();
+
+            xWriter.WriteStartElement("amount");
+            xWriter.WriteString("50");
+            xWriter.WriteEndElement();
+
+            xWriter.WriteEndElement();
+            xWriter.WriteEndElement();
+
+            xWriter.Close();
+
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("database.xml");
+
+            XmlDeclaration xmldecl;
+            xmldecl = xDoc.CreateXmlDeclaration("1.0", "UTF-8", null);
+
+            XmlElement root = xDoc.DocumentElement;
+            xDoc.InsertBefore(xmldecl, root);
+            xDoc.Save("database.xml");
+        }
+    }
+}
